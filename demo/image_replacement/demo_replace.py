729310a019ef2ac8a46c8d1699a4f8b62c1abfba
Flaze <18499438+FlazeNaive@users.noreply.github.com>
2023-05-30 13:59:23 +0200

    done check and fixed bugs resulted by changing of path

--- demo/image_replacement/demo_replace.py
+++ demo/image_replacement/demo_replace.py
@@ -11,13 +11,13 @@ from retrieve.retriever import retrieve_API, load_images, train_model
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", help="path to model", default="retrieve/model.pkl")
+    parser.add_argument("--model", help="path to model", default="./model.pkl")
     parser.add_argument("--dataset", help="path to dataset", default="retrieve/dataset_demo")
     parser.add_argument("--shapes", help="path to shapes.pkl", 
                         default="../results/previous_results/clip/exp1/pkls/clip_shapes.pkl")
     parser.add_argument("--shapes_groups", help="path to shape_groups.pkl", 
                         default="../results/previous_results/clip/exp1/pkls/clip_shape_groups.pkl")
-    parser.add_argument("--output", help="name of output image", default="../results/photomosaic/result.png")
+    parser.add_argument("--output", help="name of output image", default="result.png")
     args = parser.parse_args()
     
     model, images = prepare_model(args.model, args.dataset)
