b32e37a461e916873299228345602dfc818eb9c3
su-xingyu <suxingyu98@gmail.com>
2023-05-31 23:59:39 +0200

    fix typo

--- demo/image_replacement/demo_replace.py
+++ demo/image_replacement/demo_replace.py
@@ -15,13 +15,13 @@ if __name__ == "__main__":
     parser.add_argument("--dataset", help="path to dataset", default="retrieve/dataset_demo")
     parser.add_argument("--shapes", help="path to shapes.pkl", 
                         default="../results/previous_results/clip/exp1/pkls/clip_shapes.pkl")
-    parser.add_argument("--shapes_groups", help="path to shape_groups.pkl", 
+    parser.add_argument("--shape_groups", help="path to shape_groups.pkl", 
                         default="../results/previous_results/clip/exp1/pkls/clip_shape_groups.pkl")
     parser.add_argument("--output", help="name of output image", default="result.png")
     args = parser.parse_args()
     
     model, images = prepare_model(args.model, args.dataset)
-    tiles = read(args.shapes, args.shapes_groups)
+    tiles = read(args.shapes, args.shape_groups)
     outname = args.output
     if not (outname.endswith(".png") or outname.endswith(".jpg")):
         outname += ".png"
